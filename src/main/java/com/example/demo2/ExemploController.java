451dea965a0480de80a8805483ff640bac434a71
Tarcisio Blois <tarcisio.b.neto@gmail.com>
2021-04-01 16:31:47 -0300

    "definindo padrão MVC - criando model"

--- src/main/java/com/example/demo2/ExemploController.java
+++ src/main/java/com/example/demo2/ExemploController.java
@@ -5,19 +5,28 @@
  */
 package com.example.demo2;
 
+import java.time.LocalDate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *
  * @author tbn
  */
-@Controller
+@Controller //clase controller criada
 public class ExemploController {
 
-    
-    @GetMapping
+    @GetMapping("/ex1")
     public String exemplo1() {
         return "exemplo1";
+    }
+
+    @GetMapping("/ex2")
+    public String exemplo2(Model model) {
+        model.addAttribute("nome", "Fulano");
+        model.addAttribute("data nasci", LocalDate.parse("2000-01-01"));
+        model.addAttribute("numero", 10);
+        return "exemplo2";
     }
 }
